4a73f75bb50e569f15738946b6d0871a296d04dd
MiguelAAvila <migzavila01@gmail.com>
2022-03-12 14:29:20 -0600

    removed localstorage

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {users} from '../../../data/users';
 
 export default function handler(req, res) {
-  res.status(200).json({ miguel: "migz", ferdz: "ferdz" });
+  res.status(200).json(users );
 }

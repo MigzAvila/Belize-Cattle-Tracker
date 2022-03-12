59a6d05f2293a08302f6f93ae681d2c937be1059
Ferdi Heredia <ferheredia99@gmail.com>
2022-03-12 14:21:41 -0600

    working on contact us page

--- pages/MainPages/ContactUs.js
+++ pages/MainPages/ContactUs.js
@@ -17,10 +17,15 @@ const ContactUs = () => {
   return (
     <>
       {/* image contact form */}
-      <div style={{ borderColor: "black", width: "60%", margin: "0 auto" }}><br/>
+      <div
+        className={contactUsClass.body}
+        style={{ borderColor: "black", width: "60%", margin: "0 auto" }}
+      >
+        <br />
         <div className={contactUsClass.formHeader}>
           <h1 className={contactUsClass.h1}>
-            <span className={contactUsClass.emailEnvelope}>&nbsp;</span>Contact Us
+            <span className={contactUsClass.emailEnvelope}>&nbsp;</span>Contact
+            Us
           </h1>
           <div className={contactUsClass.formHeader}>
             <h1 aria-hidden="true" role="presentation"></h1>
@@ -38,191 +43,162 @@ const ContactUs = () => {
           novalidate="novalidate"
           className={contactUsClass.form}
         >
-          <Grid sx={{ borderColor: "black" }}>
-            <ul className={contactUsClass.formUl}>
-              <li id="formFirstName" className={contactUsClass.formUlLi}>
-                <div className={contactUsClass.formField}>
-                  {/* <div class="label placeholder">First Name</div> */}
-                  <input
-                    className={contactUsClass.input}
-                    aria-label="First Name"
-                    name="fname"
-                    placeholder="First Name"
-                    type="text"
-                    // value=""
-                    aria-required="true"
-                    autocomplete="given-name"
-                  />
-                  <div className={contactUsClass.formRequired}>*</div>
-                </div>
-                <div class="formError"></div>
-              </li>
-              <li id="formLastName" className={contactUsClass.formUlLi}>
-                <div className={contactUsClass.formField}>
-                  {/* <div class="label placeholder">Last Name</div> */}
-                  <input
-                    className={contactUsClass.input}
-                    aria-label="Last Name"
-                    name="lname"
-                    type="text"
-                    // value=""
-                    placeholder="Last Name"
-                    aria-required="true"
-                    autocomplete="family-name"
-                  />
-                  <div className={contactUsClass.formRequired}>*</div>
-                </div>
-                <div class="formError"></div>
-              </li>
-              <li id="formEmail" className={contactUsClass.formUlLi}>
-                <div className={contactUsClass.formField}>
-                  {/* <div class="label placeholder">Email</div> */}
-                  <input
-                    className={contactUsClass.input}
-                    aria-label="Email"
-                    name="email"
-                    type="text"
-                    // value=""
-                    placeholder="Email"
-                    aria-required="true"
-                    autocomplete="email"
-                  />
-                  <div className={contactUsClass.formRequired}>*</div>
-                </div>
-                <div class="formError"></div>
-              </li>
-              <li id="formTelephone" className={contactUsClass.formUlLi}>
-                <div className={contactUsClass.formField}>
-                  {/* <div class="label placeholder">Phone</div> */}
-                  <input
-                    className={contactUsClass.input}
-                    aria-label="Phone"
-                    name="telephone"
-                    type="text"
-                    // value=""
-                    placeholder="Phone"
-                    autocomplete="tel"
-                  />
-                  <div className={contactUsClass.formRequired}></div>
-                </div>
-                <div class="formError"></div>
-              </li>
-              {/* <!-- FH - New Fields added - 03/10/2022 --> */}
-              <li id="formPostalCode" className={contactUsClass.formUlLi}>
-                <div className={contactUsClass.formField}>
-                  {/* <div class="label placeholder">Postal Code</div> */}
-                  <input
-                    className={contactUsClass.input}
-                    aria-label="Postal Code"
-                    name="Postal Code"
-                    type="text"
-                    // value=""
-                    placeholder="Postal Code"
-                  />
-                </div>
-              </li>
-              <li id="formCity" className={contactUsClass.formUlLi}>
-                <div className={contactUsClass.formField}>
-                  {/* <div class="label placeholder">City</div> */}
-                  <input
-                    className={contactUsClass.input}
-                    aria-label="City"
-                    name="City"
-                    type="text"
-                    // value=""
-                    placeholder="City"
-                    autocomplete="address-level2"
-                  />
-                </div>
-              </li>
-              {/* <!-- end of new fields added --> */}
-              <li id="formLocations" className={contactUsClass.formUlLi}>
-                <div
-                  className={[
-                    contactUsClass.formField,
-                    contactUsClass.formLocations,
-                  ]}
-                >
-                  <div class="label placeholder"></div>
-                  <select
-                    className={contactUsClass.select}
-                    name="location"
-                    id="location"
-                    class="DealerLocation"
-                    aria-label="Select A Location"
-                    aria-required="true"
+          <Grid
+            container
+            rowspacing="1"
+            columnspacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            <Grid item xs={6}>
+              <Item>
+                <ul className={contactUsClass.formUl}>
+                  <li id="formFirstName" className={contactUsClass.formUlLi}>
+                    <div className={contactUsClass.formField}>
+                      {/* <div class="label placeholder">First Name</div> */}
+                      <input
+                        className={contactUsClass.input}
+                        aria-label="First Name"
+                        name="fname"
+                        placeholder="First Name"
+                        type="text"
+                        // value=""
+                        aria-required="true"
+                        autocomplete="given-name"
+                      />
+                      <div className={contactUsClass.formRequired}>*</div>
+                    </div>
+                    <div class="formError"></div>
+                  </li>
+                  <li id="formLastName" className={contactUsClass.formUlLi}>
+                    <div className={contactUsClass.formField}>
+                      {/* <div class="label placeholder">Last Name</div> */}
+                      <input
+                        className={contactUsClass.input}
+                        aria-label="Last Name"
+                        name="lname"
+                        type="text"
+                        // value=""
+                        placeholder="Last Name"
+                        aria-required="true"
+                        autocomplete="family-name"
+                      />
+                      <div className={contactUsClass.formRequired}>*</div>
+                    </div>
+                    <div class="formError"></div>
+                  </li>
+                  <li id="formEmail" className={contactUsClass.formUlLi}>
+                    <div className={contactUsClass.formField}>
+                      {/* <div class="label placeholder">Email</div> */}
+                      <input
+                        className={contactUsClass.input}
+                        aria-label="Email"
+                        name="email"
+                        type="text"
+                        // value=""
+                        placeholder="Email"
+                        aria-required="true"
+                        autocomplete="email"
+                      />
+                      <div className={contactUsClass.formRequired}>*</div>
+                    </div>
+                    <div class="formError"></div>
+                  </li>
+                  <li id="formTelephone" className={contactUsClass.formUlLi}>
+                    <div className={contactUsClass.formField}>
+                      {/* <div class="label placeholder">Phone</div> */}
+                      <input
+                        className={contactUsClass.input}
+                        aria-label="Phone"
+                        name="telephone"
+                        type="text"
+                        // value=""
+                        placeholder="Phone"
+                        autocomplete="tel"
+                      />
+                      <div className={contactUsClass.formRequired}></div>
+                    </div>
+                    <div class="formError"></div>
+                  </li>
+                  {/* <!-- FH - New Fields added - 03/10/2022 --> */}
+                  <li id="formPostalCode" className={contactUsClass.formUlLi}>
+                    <div className={contactUsClass.formField}>
+                      {/* <div class="label placeholder">Postal Code</div> */}
+                      <input
+                        className={contactUsClass.input}
+                        aria-label="Postal Code"
+                        name="Postal Code"
+                        type="text"
+                        // value=""
+                        placeholder="Postal Code"
+                      />
+                    </div>
+                  </li>
+                  <li id="formCity" className={contactUsClass.formUlLi}>
+                    <div className={contactUsClass.formField}>
+                      <input
+                        className={contactUsClass.input}
+                        aria-label="City"
+                        name="City"
+                        type="text"
+                        // value=""
+                        placeholder="City"
+                        autocomplete="address-level2"
+                      />
+                    </div>
+                  </li>
+                  <li
+                    id="formComments"
+                    role="presentation"
+                    className={contactUsClass.formUlLi}
                   >
-                    <option value="">Select A Location</option>
-                    <option value="Windsor">Windsor</option>
-                    <option value="Aylesford">Aylesford</option>
-                    <option value="Moncton">Moncton</option>
-                  </select>
-                </div>
-                <div class="formError"></div>
-              </li>
-            </ul>
-            <ul class="ulComments" role="presentation">
-              <li
-                id="formComments"
-                role="presentation"
-                className={contactUsClass.formUlLi}
-              >
-                <div className={contactUsClass.formField}>
-                  {/* <div className={contactUsClass.label}>Comments/Questions</div> */}
-                  <textarea
-                    className={contactUsClass.textarea}
-                    aria-label="Comments/Questions"
-                    name="comments"
-                    placeholder="Comments/Questions"
-                  ></textarea>
-                  <div className={contactUsClass.formRequired}></div>
-                </div>
-                <div class="formError"></div>
-              </li>
-            </ul>
-            <div id="newsletter-subscribe">
-              <input
-                className={contactUsClass.inputCheckBox}
-                aria-label="Newsletter Opt-In"
-                name="NewsletterOptIn"
-                type="checkbox"
-                value="Y"
-                aria-labelledby="newsletter-subscribe-label"
-              />{" "}
-              &nbsp;
-              <span id="newsletter-subscribe-label">
-                Yes, I would like to receive commercial information from Nova
-                International and Kubota Canada Ltd. about Kubota products,
-                including promotions, offers, news, and customer satisfaction
-                material! I understand I can later unsubscribe. Consent is
-                sought by the Dealer for itself and for Kubota Canada. Kubota
-                Canada Ltd. 5900 14th Avenue Markham, ON, L3S 4K4,{" "}
-                <a href="mailto: kcl_g.customerservice@kubota.com">
-                  kcl_g.customerservice@kubota.com
-                </a>
-                . You may view the Kubota Canada Privacy Policy at:{" "}
-                <a href="/read-our-privacy-statement--xprivacy">
-                  Privacy Policy
-                </a>
-                .
-              </span>
-            </div>{" "}
-            <ul className={contactUsClass.submitBlock} role="presentation">
-              <li class={contactUsClass.submitBlockLi} role="presentation">
-                <button
-                  className={contactUsClass.submitBlockLiButton}
-                  id="btnSubmit"
-                  name="submit"
-                  type="submit"
-                  value="Submit"
-                >
-                  Submit
-                </button>
-              </li>
-            </ul>
+                    <div className={contactUsClass.formField}>
+                      <textarea
+                        className={contactUsClass.textarea}
+                        aria-label="Comments/Questions"
+                        name="comments"
+                        placeholder="Comments/Questions"
+                      ></textarea>
+                    </div>
+                  </li>
+                  {/* <!-- end of new fields added --> */}
+                </ul>
+                <div id="newsletter-subscribe">
+                  <input
+                    className={contactUsClass.inputCheckBox}
+                    aria-label="Newsletter Opt-In"
+                    name="NewsletterOptIn"
+                    type="checkbox"
+                    value="Y"
+                    aria-labelledby="newsletter-subscribe-label"
+                  />{" "}
+                  &nbsp;
+                  <span id="newsletter-subscribe-label">
+                    Subscribe to newsletter
+                  </span>
+                </div>{" "}
+                <ul className={contactUsClass.submitBlock} role="presentation">
+                  <li class={contactUsClass.submitBlockLi} role="presentation">
+                    <button
+                      className={contactUsClass.submitBlockLiButton}
+                      id="btnSubmit"
+                      name="submit"
+                      type="submit"
+                      value="Submit"
+                    >
+                      Submit
+                    </button>
+                  </li>
+                </ul>
+              </Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>Hello2</Item>
+            </Grid>
           </Grid>
         </form>
-        <br /><br /><br />
+
+        <br />
+        <br />
+        <br />
       </div>
     </>
   );

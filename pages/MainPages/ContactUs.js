eb96dd61086ae5079e0660a8d9e9d5414bca382e
MiguelAAvila <migzavila01@gmail.com>
2022-03-27 17:40:16 -0600

    Clean contact us page

--- pages/MainPages/ContactUs.js
+++ pages/MainPages/ContactUs.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
 import contactUsClass from "../../styles/ContactUs.module.css";
 import CustomerNav from "../components/CustomerNavBar/index";
 
@@ -193,41 +192,5 @@ const ContactUs = () => {
     </>
   );
 };
-// const ContactUs = () => {
-//   return (
-//     <>
-//       {/* image contact form */}
-//       <Box
-//         sx={{
-//           width: "auto",
-//           height: "auto",
-//           paddingTop: "50px",
-//           backgroundRepeat: "no-repeat",
-//           backgroundSize: "cover",
-//           backgroundPosition: "center",
-//           paddingBottom: "50px",
-//         }}
-//       >
-//         <Grid
-//           container
-//           justifyContent="center"
-//           rowSpacing={1}
-//           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-//           sx={{ paddingTop: "30px" }}
-//         >
-//           <Grid item xs={10}>
-//             <Item sx={{ fontSize: "30px", fontWeight: "bold" }}>
-//               GET IN TOUCH
-//             </Item>
-//             <Item sx={{ fontSize: "15px" }}>
-//               If you have any question, you can contact Us lorem
-//               aslfjsalkdfhskldf lksfjslkf sldfh sdajfhsa flkasf hsdkjf
-//             </Item>
-//           </Grid>
-//         </Grid>
-//       </Box>
-//     </>
-//   );
-// };
 
 export default ContactUs;

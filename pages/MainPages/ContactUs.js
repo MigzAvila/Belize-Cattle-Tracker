47d27e57f80e835d81c81a42ececa51b51decb68
ninjafrombz <92419198+ninjafrombz@users.noreply.github.com>
2022-04-02 16:26:30 -0600

    .

--- pages/MainPages/ContactUs.js
+++ pages/MainPages/ContactUs.js
@@ -192,5 +192,45 @@ const ContactUs = () => {
     </>
   );
 };
+<<<<<<< Updated upstream
+=======
+
+// const ContactUs = () => {
+//   return (
+//     <>
+//       {/* image contact form */}
+//       <Box
+//         sx={{
+//           width: "auto",
+//           height: "auto",
+//           paddingTop: "50px",
+//           backgroundRepeat: "no-repeat",
+//           backgroundSize: "cover",
+//           backgroundPosition: "center",
+//           paddingBottom: "50px",
+//         }}
+//       >
+//         <Grid
+//           container
+//           justifyContent="center"
+//           rowSpacing={1}
+//           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+//           sx={{ paddingTop: "30px" }}
+//         >
+//           <Grid item xs={10}>
+//             <Item sx={{ fontSize: "30px", fontWeight: "bold" }}>
+//               GET IN TOUCH
+//             </Item>
+//             <Item sx={{ fontSize: "15px" }}>
+//               If you have any question, you can contact Us lorem
+//               aslfjsalkdfhskldf lksfjslkf sldfh sdajfhsa flkasf hsdkjf
+//             </Item>
+//           </Grid>
+//         </Grid>
+//       </Box>
+//     </>
+//   );
+// };
+>>>>>>> Stashed changes
 
 export default ContactUs;

041363c96c3005299bc98faba01e4f6403a82aa4
MiguelAAvila <migzavila01@gmail.com>
2022-04-03 21:11:51 -0600

    Refactor code and made it useable

--- pages/MainPages/create_cattle_info.js
+++ pages/MainPages/create_cattle_info.js
@@ -6,30 +6,29 @@ import {collection, getDocs, addDoc} from "firebase/firestore";
 
 function createInfo() {
 
-    const [newCattleID, setNewCattleID] = useState(0);
-    const [newCattleAntbio, setNewCattleAntbio] = useState("");
-    const [newCattleBreed, setNewCattleBreed] = useState("");
-    const [newCattleGender, setNewCattleGender] = useState("");
-    const [newCattleWeight, setNewCattleWeight] = useState("");
-    const [newCattleDna, setNewCattleDna] = useState("");
-    const [newCattleTag, setNewCattleTag] = useState("");
-    const [newCattleFarmer, setNewCattleFarmer] = useState("");
-    const [newCattleLocation, setNewCattleLocation] = useState("");
-    const [newCattleHissue, setNewCattleHissue] = useState("");
-    const [newCattleReartype, setNewCattleRearType] = useState("");
-    const [newCattleReproStat, setNewCattleReproStat] = useState("");
+    const [newCattleInfo, setNewCattleInfo] = useState({
+    newCattleID: 0,
+    newCattleAntbio: "",
+    newCattleBreed: "",
+    newCattleGender: "",
+    newCattleWeight: "",
+    newCattleDna: "",
+    newCattleTag: "",
+    newCattleFarmer: "",
+    newCattleLocation: "",
+    newCattleHissue: "",
+    newCattleReartype: "",
+    newCattleReproStat: "",
 
+  });
 
-
-    const [cattle_info, setCattleInfo] = useState([]);
+    const apiService = require("../components/APIcalls/APIService");
+    const [cattleInfo, setcattleInfo] = useState([]);
     const cattleInfoCollection = collection(db, "cattle_info");
 
     const createCattle = async () => {
-        await addDoc(cattleInfoCollection, {cattle_id: newCattleID, antbio_type: newCattleAntbio, breed: newCattleBreed,
-        gender: newCattleGender, cattle_weight: newCattleWeight, dna_type: newCattleDna, eartag_code: newCattleTag,
-        farmer: newCattleFarmer, location: newCattleLocation, und_hlth_issues: newCattleHissue,
-        rearing_type: newCattleReartype, repro_stat: newCattleReproStat
-    })
+        let status = await apiService.addNewCattle(newCattleInfo);
+        console.log(status);
     };
     
     //use effect called whenever the page renders and gets the cattle info and displays
@@ -37,58 +36,32 @@ function createInfo() {
 
         const getCattleInfo = async () => {
             const data = await getDocs(cattleInfoCollection);
-            setCattleInfo(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+            setcattleInfo(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
         };
 
         getCattleInfo();
     }, [])
 
-
-    //this should display the collection of cattle_info information
     return (
     <div className="createInfo">
-        <input type="number" placeholder="Cattle id..." onChange={(event) =>{
-            setNewCattleID(event.target.value);
-        }}/>
-        <input placeholder="AntiBio..." onChange={(event) =>{
-            setNewCattleAntbio(event.target.value);
-        }}/>
-        <input placeholder="Breed..." onChange={(event) =>{
-            setNewCattleBreed(event.target.value);
-        }}/>
-        <input placeholder="Gender..." onChange={(event) =>{
-            setNewCattleGender(event.target.value);
-        }}/>
-        <input placeholder="Weight..." onChange={(event) =>{
-            setNewCattleWeight(event.target.value);
-        }}/>
-        <input placeholder="DNA..." onChange={(event) =>{
-            setNewCattleDna(event.target.value);
-        }}/>
-        <input placeholder="Tag..." onChange={(event) =>{
-            setNewCattleTag(event.target.value);
-        }}/>
-        <input placeholder="Farmer..." onChange={(event) =>{
-            setNewCattleFarmer(event.target.value);
-        }}/>
-        <input placeholder="Location..." onChange={(event) =>{
-            setNewCattleLocation(event.target.value);
-        }}/>
-        <input placeholder="Health Issue..." onChange={(event) =>{
-            setNewCattleHissue(event.target.value);
-        }}/>
-        <input placeholder="Rearing Type..." onChange={(event) =>{
-            setNewCattleRearType(event.target.value);
-        }}/>
-        <input placeholder="Rerpoduction Status..." onChange={(event) =>{
-            setNewCattleReproStat(event.target.value);
-        }}/>
+        <input type="number" placeholder="Cattle id..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleID: e.target.value})} value={newCattleInfo.newCattleID} />
+        <input placeholder="AntiBio..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleAntbio: e.target.value})} value={newCattleInfo.newCattleAntbio}/>
+        <input placeholder="Breed..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleBreed: e.target.value})} value={newCattleInfo.newCattleBreed}/>
+        <input placeholder="Gender..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleGender: e.target.value})} value={newCattleInfo.newCattleGender} />
+        <input placeholder="Weight..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleWeight: e.target.value})} value={newCattleInfo.newCattleWeight}/>
+        <input placeholder="DNA..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleDna: e.target.value})} value={newCattleInfo.newCattleDna}/>
+        <input placeholder="Tag..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleTag: e.target.value})} value={newCattleInfo.newCattleTag}/>
+        <input placeholder="Farmer..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleFarmer: e.target.value})} value={newCattleInfo.newCattleFarmer}/>
+        <input placeholder="Location..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleLocation: e.target.value})} value={newCattleInfo.newCattleLocation}/>
+        <input placeholder="Health Issue..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleHissue: e.target.value})} value={newCattleInfo.newCattleHissue}/>
+        <input placeholder="Rearing Type..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleReartype: e.target.value})} value={newCattleInfo.newCattleReartype}/>
+        <input placeholder="Rerpoduction Status..." onChange={(e) => setNewCattleInfo({...newCattleInfo, newCattleReproStat: e.target.value})} value={newCattleInfo.newCattleReproStat}/>
         
 
         <button onClick={createCattle}> Create Cattle</button>
-        {cattle_info.map((cattle) =>{
+        {cattleInfo.map((cattle, index) =>{
             return(
-                    <table striped bordered hover size="sm">
+                    <table striped bordered hover size="sm" key= {index}>
                     <thead>
                         <tr>
                             <th>Cattle Id:</th>
--- pages/components/APIcalls/APIService.js
+++ pages/components/APIcalls/APIService.js
@@ -1,3 +1,7 @@
+
+import {db} from '../../firebase-config';
+import {collection, getDocs, addDoc} from "firebase/firestore";
+
 const APIService = {
 
    getFirebaseKey() {
@@ -11,6 +15,30 @@ const APIService = {
       .then((data) => {
         return data;
       });
+  },
+
+  async addNewCattle (newCattleInfo){
+      let status = false;
+            try {
+               if (newCattleInfo.newCattleID !== 0 || newCattleInfo.newCattleAntbio !== "" || newCattleInfo.newCattleBreed !== "" || newCattleInfo.newCattleGender !== "" || newCattleInfo.newCattleWeight !== "" || newCattleInfo.newCattleDna !== "" || newCattleInfo.newCattleTag !== "" || newCattleInfo.newCattleFarmer !== "" || newCattleInfo.newCattleLocation !== "" || newCattleInfo.newCattleHissue !== "" || newCattleInfo.newCattleReartype !== "" || newCattleInfo.newCattleReproStat !== "") {
+                status = true;
+               }
+               else {
+                   status = false;
+                   throw new Error("Please fill in all the fields");
+               }
+            }
+            catch (err) {
+                console.log(err);
+                return false; 
+            }
+        status = true;
+        if (status) {
+          await addDoc(cattleInfoCollection, {cattle_id: newCattleInfo.newCattleID, antbio_type: newCattleInfo.newCattleAntbio, breed: newCattleInfo.newCattleBreed,
+        gender: newCattleInfo.newCattleGender, cattle_weight: newCattleInfo.newCattleWeight, dna_type: newCattleInfo.newCattleDna, eartag_code: newCattleInfo.newCattleTag,
+        farmer: newCattleInfo.newCattleFarmer, location: newCattleInfo.newCattleLocation, und_hlth_issues: newCattleInfo.newCattleHissue,
+        rearing_type: newCattleInfo.newCattleReartype, repro_stat: newCattleInfo.newCattleReproStat    })
+        }
   },
 
  // updating user

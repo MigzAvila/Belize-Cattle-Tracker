25f1d814ae9a54e0f0af46a0b30a2f3f4451f562
MiguelAAvila <migzavila01@gmail.com>
2022-04-05 18:49:49 -0600

    Comment test case

--- pages/components/APIcalls/APIService.test.js
+++ pages/components/APIcalls/APIService.test.js
@@ -2,6 +2,7 @@ const APIService = require( "./APIService")
 
 let newCattleInfo = {}
 
+//create a valid info object for cattle
 function getValidteInfo() {
    return newCattleInfo = {
       newCattleID: 100,
@@ -19,6 +20,7 @@ function getValidteInfo() {
    }
 }
 
+//create a empty obj for a cattle 
 function initialize() {
    newCattleInfo = {
          newCattleID: 0,
@@ -36,33 +38,33 @@ function initialize() {
       }
 }
 
+//Clear cattle obj
 function clear() {
    newCattleInfo = {}
 }
 
+//Setup function for each test case
 beforeEach(() => {
    initialize()   
 })
 
 
+//tear down function after each test case run
 afterEach(() => {
      clear()
 })
 
 
+//Test Suites for API Service files 
 describe('Test api calls', () => {
+
+   //Test case to check that there is a value for firebase
    test('Test to see if we have a value for the firebase key', () => {
       expect(APIService.getFirebaseKey).toBeTruthy();
    
    });
 
-      
-   test('Add cattle info fails to the database with invalid data', async () => { 
-   const result = await APIService.addNewCattle(newCattleInfo);
-   expect(result).toBe(false);
-   
-  } );
-
+   //Test case to check cattle info successfully to the database with valid data
    test('Add cattle info successfully to the database', async () => { 
    newCattleInfo = getValidteInfo()
    const result = await APIService.addNewCattle(newCattleInfo);
@@ -70,6 +72,14 @@ describe('Test api calls', () => {
 
    });
 
+   //Test case to check cattle info fails to the database with invalid data
+   test('Add cattle info fails to the database with invalid data', async () => { 
+   const result = await APIService.addNewCattle(newCattleInfo);
+   expect(result).toBe(false);
+   
+  } );
+
+   //Test case to check  product info successfully to the database with valid data
    test('Add product info successfully to the database', async () => { 
    
       newCattleInfo = {
@@ -95,6 +105,7 @@ describe('Test api calls', () => {
 
    });
 
+   //Test case to check  product info successfully to the database with invalid data
    test('Add product info fails to the database with invalid data', async () => { 
 
    const result = await APIService.addNewProduct(newCattleInfo);
@@ -103,7 +114,7 @@ describe('Test api calls', () => {
    });
 
 
-
+   //Test case to check Slaughter info successfully to the database with valid data
    test('Add Slaughter info successfully to the database', async () => { 
       newCattleInfo = {
         newCattleID : 100023 , 
@@ -117,7 +128,7 @@ describe('Test api calls', () => {
    expect(result).toBe(true);
    });
 
-
+   //Test case to check Slaughter info successfully to the database with invalid data
    test('Add Slaughter info fails to post to the database with invalid data', async () => { 
       newCattleInfo = {
       newCattleID : 0 , 

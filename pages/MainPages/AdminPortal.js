0a5fe5a0bb891d8f0130c51bdbce6fd9ab1da7f1
MiguelAAvila <migzavila01@gmail.com>
2022-05-08 17:54:58 -0600

    Finished Request work

--- pages/MainPages/AdminPortal.js
+++ pages/MainPages/AdminPortal.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Button, Grid, Card, CardContent, Typography} from '@material-ui/core'
 import PortalNav from "../components/RoleNavBars/PortalNavBar";
+import Link from 'next/Link';
 
 
 	const btnStyle = { margin: '15px 0', height: 60, width: 250 }
@@ -20,13 +21,20 @@ class adminPortal extends React.Component {
 						</Typography>
 						<Grid container spacing={1} align="center">
 							<Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle}>Create Cattle Profile</Button>
+								<Link href="/MainPages/add_cattle_info">
+									<Button color='primary' variant='contained' style={btnStyle}>Create Cattle Profile</Button>	
+								</Link>
 							</Grid>
 							<Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle}>Search Cattle Profile</Button>
+								<Link href="/MainPages/SearchCattle">
+								 	<Button color='primary' variant='contained' style={btnStyle}>Search Cattle Profile</Button>
+								</Link>
 							</Grid>
                             <Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle}>Generate Reports</Button>
+								<Link href="/MainPages/AdminReportsPortal">
+									<Button color='primary' variant='contained' style={btnStyle}>Generate Reports</Button>
+								</Link>
+								
 							</Grid>
 						</Grid>
 					</CardContent>

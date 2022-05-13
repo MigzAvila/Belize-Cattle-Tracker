d1981a0fc0e5887cebdbe4e12c189bcadd73641a
MiguelAAvila <migzavila01@gmail.com>
2022-05-12 18:58:43 -0600

    Fix refreshing issue

--- pages/MainPages/MainMenu.js
+++ pages/MainPages/MainMenu.js
@@ -1,13 +1,24 @@
 import React from "react";
+import {useEffect} from "react"
 import { Button, Grid, Card, CardContent, Typography } from '@material-ui/core'
 import Link from "next/link";
 
-
+// export [showRole, setShowRole] = useState("")
 export const Roles = "";
 
 const mainPortal = () => {
 
 	const btnStyle = { margin: '15px 0', height: 60 }
+	useEffect(() => {
+		window.localStorage.setItem("Role", JSON.stringify(Roles))
+
+, [Roles]	})
+
+const setRole = (role) => {
+	Roles = role
+	window.localStorage.setItem("Role", JSON.stringify(Roles))
+}
+
 
 	return (
 		<div className="mainMenu" style={{marginTop: '30px'}}>
@@ -19,28 +30,28 @@ const mainPortal = () => {
 						</Typography>
 						<Grid container spacing={1}>
 							<Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle} onClick={() => Roles="Farmer"} fullWidth>
+								<Button color='primary' variant='contained' style={btnStyle} onClick={() => setRole("Farmer")} fullWidth>
 									<Link href="/MainPages/FarmerPortal">
 										Birth Stage Info: Farmer/BAHA
 									</Link>
 								</Button>
 							</Grid>
 							<Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle} onClick={() => Roles="Slaughter"} fullWidth>
+								<Button color='primary' variant='contained' style={btnStyle} onClick={() => setRole("Slaughter")} fullWidth>
 									<Link href="/MainPages/SlaughterManagerPortal">
 										Slaughter Stage: Slaughterhouse Manager
 									</Link>
 								</Button>
 							</Grid>
 							<Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle} onClick={() => Roles="Product"} fullWidth>
+								<Button color='primary' variant='contained' style={btnStyle} onClick={() => setRole("Product")} fullWidth>
 									<Link href="/MainPages/ProductManagerPortal">
 										Processing/Packaging Stage: Production/Packaging Manager
 									</Link>
 								</Button>
 							</Grid>
 							<Grid xs={12}>
-								<Button color='primary' variant='contained' style={btnStyle} onClick={() => Roles="Admin"} fullWidth>
+								<Button color='primary' variant='contained' style={btnStyle} onClick={() => setRole("Admin")} fullWidth>
 									<Link href="/MainPages/AdminPortal">
 										ADMIN: BAHA
 									</Link>

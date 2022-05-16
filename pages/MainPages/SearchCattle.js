f6881a7fc14e43c7cbf63fafc898bfab489308f0
jcs-2020 <69253552+jcs-2020@users.noreply.github.com>
2022-05-16 08:38:32 -0600

    Update SearchCattle.js

--- pages/MainPages/SearchCattle.js
+++ pages/MainPages/SearchCattle.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Grid, Typography, Card, CardContent } from '@material-ui/core'
+import { Button, Grid, Typography, Card, CardContent, Input } from '@material-ui/core'
 import { styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import InputBase from '@mui/material/InputBase';
@@ -7,7 +7,10 @@ import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Link from "next/link";
+import { db } from '../firebase-config';
+import { collection, getDocs } from "firebase/firestore"; 
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 // MUI page styling 
 const Search = styled('div')(({ theme }) => ({
@@ -54,6 +57,34 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 const searchCattle = () => { //Initializing searchCattle
   const router = useRouter()
+  const [cattleInfo, setCattleInfo] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  
+  const cattleInfoCollection = collection(db, "cattle_info");
+  
+  //use effect called whenever the page renders and gets the cattle info and displays
+  useEffect(() => {
+    const getCattleInfo = async () => {
+      const data = await getDocs(cattleInfoCollection);
+      setCattleInfo(data.docs.map((doc) => ({ ...doc.data(), id: doc.cattle_id })));
+    };
+    getCattleInfo();
+  }
+  , [])
+
+  //console.log(cattleInfo)
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    let searchResult = cattleInfo.filter((cattle) => cattle.cattle_id === search);
+    setSearchResult(searchResult);
+  }
+
+  const refresh = () => {
+    setSearch("");
+    setSearchResult([]);
+  }
 
   const btnStyle = { margin: '30px 0', height: 40, width: '40%' }
   const alignBtn = { textAlign: 'center' }
@@ -61,33 +92,54 @@ const searchCattle = () => { //Initializing searchCattle
   // Page elements. Grid, Box, Search and other elements imported from MUI library.
   return (
     <div className="createInfo" style={{ marginTop: '30px' }}>
-      <Grid>
-        <Card style={{ maxWidth: 550, padding: "20px 10px", margin: "0 auto", backgroundColor: "unset" }}>
-          <IconButton>
-            <ArrowBackIcon onClick={() => router.back()} />
-          </IconButton>
-          <CardContent>
-            <Typography gutterBottom variant="h4" align="center">
-              Search Cattle Profile
-            </Typography>
-            <Typography style={{ textAlign: 'center' }}>
-              Please enter cattle ID below.
-            </Typography><br></br>
-            <Box sx={{ flexGrow: 1 }} style={borderStyle}>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-              </Search>
-            </Box>
-            <Grid style={alignBtn}>
-              <Link href='/MainPages/CattleProfile'>
-                <Button color='primary' variant='contained' style={btnStyle} fullWidth required>SEARCH</Button>
-              </Link>
-            </Grid>
-          </CardContent>
-        </Card>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Box display="flex" alignItems="center">
+            <Link href="/">
+              <IconButton>
+                <ArrowBackIcon />
+              </IconButton>
+            </Link>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+
+                placeholder="Search cattle"
+                inputProps={{ 'aria-label': 'search cattle' }}
+                onChange={handleSearch}
+                value={search}
+              />
+            </Search>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Search Results
+              </Typography>
+              <Typography variant="body2" component="p">
+                {searchResult.map((cattle) => (
+                  <div>
+                    <Typography variant="body2" gutterBottom>
+                                {searchResult.map((cattle) => (
+                                    <div>
+                                        <Link href={`/MainPages/Cattle_Profile_Details/${cattle.cattle_id}`}>                
+                                            <Button variant="contained" color="primary">
+                                                {cattle.cattle_id}
+                                            </Button>
+                                        </Link>
+                                    </div>
+                                ))}
+                            </Typography>
+                  </div>
+                ))}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </div>
   )
